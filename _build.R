b4d80edaabe5b8e0741f8fb0c97788d48742338d
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-12-18 11:29:02 +0000

    give time for snow to fall

--- _build.R
+++ _build.R
@@ -1,4 +1,4 @@
 # setwd() to this directory
-pagedown::chrome_print("index.Rmd", async = FALSE)
+pagedown::chrome_print("index.Rmd", async = FALSE, wait = 5)
 
 # Setting async=TRUE may help for slides that need more time to compile. See also timeout=<value>.
